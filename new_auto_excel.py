f2cb602d97f9e4e88850f9b0b3c2d6fa40ae48d4
anjhon <anjhon@163.com>
2021-03-19 20:07:08 +0800

    更新自动填表文件的月数据填表

--- new_auto_excel.py
+++ new_auto_excel.py
@@ -3,23 +3,38 @@ import os
 import sys
 from openpyxl import load_workbook   # 读取表的模块
 
-base_path = sys.path[0] + '\\周数据表\\'
-finaly_workbook = load_workbook(sys.path[0] + '\\表格\\周平衡计分卡.xlsx')
+base_path = sys.path[0]
+finaly_workbook_week = load_workbook(base_path + '\\表格\\周平衡计分卡.xlsx')
+finaly_workbook_month = load_workbook(base_path + '\\表格\\月平衡计分卡.xlsx')
 
 
 # take all tables
-def take_tables(base_path):
-    dirs = os.listdir(base_path)
-    dir_sheetdir = []
-    for dir in dirs:
-        names = os.listdir(base_path + dir)
-        sheetdir_tables = []
-        for name in names:
-            table = load_workbook(base_path + dir + "\\"+name)
-            sheetdir_tables.append(table)
-        dir_sheetdir.append(sheetdir_tables)
+def take_tables(data_path):
+    dirs = os.listdir(data_path)  # 获取路径下所有文件和文件夹名称
+    is_exist_dir = []
+    for i in dirs:
+        if os.path.isdir(data_path+'\\'+i):
+            is_exist_dir.append(i)
 
-    return dir_sheetdir
+    if len(is_exist_dir) > 0:
+        week_data_dirs_excels = []
+        for dir in dirs:
+            excel_names = os.listdir(data_path + dir)
+            week_data_excels = []
+            for name in excel_names:
+                table = load_workbook(data_path + dir + "\\"+name)
+                week_data_excels.append(table)
+            week_data_dirs_excels.append(week_data_excels)
+
+        return week_data_dirs_excels
+
+    else:
+        month_excels = []
+        for month_table_name in dirs:
+            month_table = load_workbook(data_path + "\\"+month_table_name)
+            month_excels.append(month_table)
+
+        return month_excels
 
 
 # the review_data
@@ -64,29 +79,42 @@ def table_to_AIsheet(tables, finaly_table_sheet):
                 row=i, column=5, value=finaly_table_sheet.cell(row=i, column=4).value)
 
 
-# the main
-def main():
+# the week_data
+def week_data():
     # 读取汇总表的三个sheet
-    sheet1_finaly_workbook = finaly_workbook[finaly_workbook.sheetnames[0]]
-    sheet2_finaly_workbook = finaly_workbook[finaly_workbook.sheetnames[1]]
-    sheet3_finaly_workbook = finaly_workbook[finaly_workbook.sheetnames[2]]
+    sheet1_finaly_workbook_week = finaly_workbook_week[finaly_workbook_week.sheetnames[0]]
+    sheet2_finaly_workbook_week = finaly_workbook_week[finaly_workbook_week.sheetnames[1]]
+    sheet3_finaly_workbook_week = finaly_workbook_week[finaly_workbook_week.sheetnames[2]]
 
     # take the table for filanySheet
-    all_tables = take_tables(base_path)
+    week_data_base_path = sys.path[0] + '\\周数据表\\'
+    all_tables = take_tables(week_data_base_path)
     sheet1_excels = all_tables[0]
     sheet2_excels = all_tables[1]
     sheet3_excels = all_tables[2]
 
     # 开始匹配填表
-    table_to_sheet(sheet1_excels, sheet1_finaly_workbook)
+    table_to_sheet(sheet1_excels, sheet1_finaly_workbook_week)
     print('sheet1填写保存完毕！！！')
-    table_to_AIsheet(sheet2_excels, sheet2_finaly_workbook)
+    table_to_AIsheet(sheet2_excels, sheet2_finaly_workbook_week)
     print('sheet2填写保存完毕！！！')
-    table_to_sheet(sheet3_excels, sheet3_finaly_workbook)
+    table_to_sheet(sheet3_excels, sheet3_finaly_workbook_week)
     print('sheet3填写保存完毕！！！')
-    finaly_workbook.save(filename=sys.path[0] + '\\表格\\周平衡计分卡.xlsx')
+    finaly_workbook_week.save(filename=sys.path[0] + '\\表格\\周平衡计分卡.xlsx')
+    print('周数据全部填写完成！')
+
+
+# the month_data
+def month_data():
+    sheet_finaly_workbook_month = finaly_workbook_month[finaly_workbook_month.sheetnames[0]]
+    month_data_base_path = sys.path[0] + '\\月数据表\\'
+    # print(month_data_base_path)
+    all_month_tables = take_tables(month_data_base_path)
+    table_to_sheet(all_month_tables, sheet_finaly_workbook_month)
+    finaly_workbook_month.save(filename=sys.path[0] + '\\表格\\月平衡计分卡.xlsx')
+    print('月数据全部填写完成！')
 
 
 if __name__ == "__main__":
-    main()
-    print('全部填写完成！')
+    week_data()
+    # month_data()
