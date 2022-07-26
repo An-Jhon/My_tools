0248ab1dbb06fb171aadb308bb7d39257a922b1d
anjhon <anjhon@163.com>
2022-07-26 14:19:35 +0800

    bug完善

--- Fund_codes/found_code.py
+++ Fund_codes/found_code.py
@@ -54,15 +54,6 @@ def timeTransformation(datetime):
 def write_history_data(history_data, sheet):
     # 历史数据写入：如果历史数据条数小于30，重新写入数据；否则将昨天的预估数据更新为实际历史数据
     if len(sheet['A'])<30:
-        # 表头写入
-        sheet['A1'] = '基金代码：' + estimate_data['fundcode']
-        sheet['A2'] = '交易日期'
-        sheet['B2'] = '单位净值'
-        sheet['C2'] = '净值涨跌'
-        sheet['D2'] = '自上次买入累计涨跌'
-        sheet['E2'] = '是否买入'
-        sheet['F2'] = '买入金额'
-        
         for r in range(0,30):
             # 日期
             date = timeTransformation(int(history_data[r]['x']/1000))
@@ -72,36 +63,50 @@ def write_history_data(history_data, sheet):
             # 涨幅
             sheet.cell(row=r+4, column=3).value = history_data[r]['equityReturn']
     else:
-        if timeTransformation(int(history_data[0]['x']/1000)) not in all_date:
-            # 净值
-            sheet.cell(row=3, column=2).value = history_data[0]['y']
-            # 涨幅
-            sheet.cell(row=3, column=3).value = history_data[0]['equityReturn']
+        latestDate = timeTransformation(int(history_data[0]['x']/1000))
+        if latestDate not in all_date:
+            sheet.insert_rows(idx=3, amount=1)
+            row = 3
+        else:
+            # 获取A列数据
+            is_A = [i.value for i in sheet['A']]
+            # 获取A列数据为1（买入点）的索引
+            is_A_index = [i for i,v in enumerate(is_A) if v==latestDate]
+            row = is_A_index[0] + 1
+        # 日期
+        sheet.cell(row=row, column=1).value = timeTransformation(int(history_data[0]['x']/1000))
+        # 净值
+        sheet.cell(row=row, column=2).value = history_data[0]['y']
+        # 涨幅
+        sheet.cell(row=row, column=3).value = history_data[0]['equityReturn']
             
             
 # 写入预估数据
 def write_estimat_data(estimate_data, sheet):
     # 如果不用重新写入，则需要将最新的历史数据和预估数据插入到表中
     if estimate_data['gztime'][:10] not in all_date:
-        print('没有数据')
         sheet.insert_rows(idx=3, amount=1)
-        # 预估数据插入
-        sheet.cell(row=3, column=1).value = estimate_data['gztime'][:10]
-        sheet.cell(row=3, column=2).value = float(estimate_data['gsz'])
-        sheet.cell(row=3, column=3).value = float(estimate_data['gszzl'])
+    # 预估数据插入
+    sheet.cell(row=3, column=1).value = estimate_data['gztime'][:10]
+    sheet.cell(row=3, column=2).value = float(estimate_data['gsz'])
+    sheet.cell(row=3, column=3).value = float(estimate_data['gszzl'])
     
     
 # 计算累计涨跌幅
 def whetherNotToBuy(sheet):
     # 获取E列数据
-    is_E = [i.value for i in sheet['E']][2:]
+    is_E = [i.value for i in sheet['E']]
     # 获取D列数据为1（买入点）的索引
     is_E_index = [i for i,v in enumerate(is_E) if v==1]
-    row = is_E_index[0] + 2
-    
+    row = is_E_index[0]   # 上次购买所在的行数的前一行
     # 累计涨跌幅度
-    for i in range(is_E_index[0]):
-        sheet[f'D{row-i}'] = f'=SUM(C{row-i}:C{row})'
+    for i in range(row-2): # 减去两行表头
+        sheet[f'D{row-i}'] = sum([j.value for j in sheet['C'][row-i-1:row]])  # row-i-1是由于sheet中切片是前闭后开的，所以要减去一行
+        if sheet[f'D{row-i}'].value >= -4:
+            sheet[f'E{row-i}'] = 0
+        else:
+            sheet[f'E{row-i}'] = 1
+        # print(sheet[f'E{row}'].value)
         
         
 codes = ['161725', '005827', '003095']
@@ -125,7 +130,16 @@ for code in codes:
     if estimate_data['name'] not in sheetnames:
         workbook.create_sheet(estimate_data['name'])
     sheet = workbook[estimate_data['name']]
-    
+
+    # 表头写入
+    sheet['A1'] = '基金代码：' + code
+    sheet['A2'] = '交易日期'
+    sheet['B2'] = '单位净值'
+    sheet['C2'] = '净值涨跌'
+    sheet['D2'] = '自上次买入累计涨跌'
+    sheet['E2'] = '是否买入'
+    sheet['F2'] = '买入金额'    
+
     # 表中已有的数据的所有日期
     all_date = [sheet['A'][a].value for a in range(len(sheet['A']))][2:]
     
